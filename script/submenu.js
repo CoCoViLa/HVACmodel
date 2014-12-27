2a5faf6fcae8a7b5fa26e071cf5959756b470b4a
Margo <margo@cs.ioc.ee>
2014-12-27 16:50:17 +0200

    Toggle menu for components and simulations

---
+++ script/submenu.js
@@ -0,0 +1,1 @@
+$(function(){     $('#componentssubmenu').hide(); $('#simulationssubmenu').hide(); $("#componentsmenu").click(function(){	$("#componentssubmenu").toggle(500, "linear");  });    $("#simulationsmenu").click(function(){	$("#simulationssubmenu").toggle(500, "linear");  });});
