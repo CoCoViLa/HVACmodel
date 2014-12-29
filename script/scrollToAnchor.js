945b8d119e8572d4937c7626a8a4779212ff0cdd
Margo <margo@cs.ioc.ee>
2014-12-29 17:11:42 +0200

    Scroll to anchor script

---
+++ script/scrollToAnchor.js
@@ -0,0 +1,1 @@
+$(function(){    $(".scroll").click(    function(event) {    	event.preventDefault();    	$('html,body').animate( { scrollTop:$(this.hash).offset().top } , 1000);    });});/*    	event.preventDefault();    	$('html,body').animate( { scrollTop:$(this.hash).offset().top } , 1000);} );*/
